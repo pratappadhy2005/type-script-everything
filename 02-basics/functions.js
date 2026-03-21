7314f7247ae826ea46eaeae93a254d5fb46b0def
Pratap Padhy <pratappadhy2005@gmail.com>
2026-03-22 09:33:47 +1100

    -Added function with union return type

--- 02-basics/functions.js
+++ 02-basics/functions.js
@@ -9,6 +9,21 @@ function getUpper(str) {
 function signUp(name, email, password) {
     return { name: name, email: email, password: password };
 }
-add(1, 2);
+var loginUser = function (email, password, name) {
+    if (name === void 0) { name = email.split('@')[0]; }
+    return { email: email, password: password, name: name };
+};
+var myValue = add(1, 2);
 getUpper('hello');
 signUp('Prata', 'prata@example.com', '123456');
+loginUser('prata@example.com');
+function getValue(myVal) {
+    if (myVal > 0) {
+        return myVal;
+    }
+    else {
+        return true;
+    }
+}
+var myResult = getValue(10);
+console.log(myResult);
--- 02-basics/functions.ts
+++ 02-basics/functions.ts
@@ -1,4 +1,4 @@
-function add(a: number, b: number) {
+function add(a: number, b: number): number {
   return a + b;
 }
 
@@ -14,9 +14,21 @@ var loginUser = (email: string, password?: string, name: string = email.split('@
   return { email, password, name };
 }
 
-add(1, 2);
+let myValue = add(1, 2);
 getUpper('hello');
 signUp('Prata', 'prata@example.com', '123456');
 loginUser('prata@example.com');  
+
+// Union Type
+function getValue(myVal: number): number | boolean {
+    if (myVal > 0) {
+        return myVal;
+    } else {
+        return true;
+    }
+}
+
+let myResult = getValue(10);
+console.log(myResult);
 
 export {}
