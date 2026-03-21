18acb00af6c78bf04ecc0d9bddb72de196667f5f
Pratap Padhy <pratappadhy2005@gmail.com>
2026-03-21 22:27:35 +1100

    -Added primitive data types

---
+++ 01-intro/variable.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greetings = 'Hello, World!';
+console.log(greetings);
+greetings.toUpperCase();
+console.log(greetings);
+// number
+var userId = 30.3;
+console.log(userId);
+// boolean
+var isAdmin = true;
+console.log(isAdmin);
---
+++ 01-intro/variable.ts
@@ -0,0 +1,16 @@
+let greetings: string = 'Hello, World!';
+
+console.log(greetings);
+
+greetings.toUpperCase();
+console.log(greetings);
+
+// number
+let userId: number = 30.3;   
+console.log(userId);
+
+// boolean
+let isAdmin: boolean = true;
+console.log(isAdmin);
+
+export {}
