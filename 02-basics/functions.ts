72d6eb2e22be6ea2a59b1265be25e24d13c66703
Pratap Padhy <pratappadhy2005@gmail.com>
2026-03-22 09:28:16 +1100

    -Added function with optional and default values

--- 02-basics/functions.ts
+++ 02-basics/functions.ts
@@ -10,9 +10,13 @@ function signUp(name: string, email: string, password: string) {
   return { name, email, password };
 }
 
+var loginUser = (email: string, password?: string, name: string = email.split('@')[0]) => {
+  return { email, password, name };
+}
 
 add(1, 2);
 getUpper('hello');
 signUp('Prata', 'prata@example.com', '123456');
+loginUser('prata@example.com');  
 
 export {}
