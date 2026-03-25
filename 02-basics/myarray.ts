2829ecce847eca9b66ae682221dc5f9cd5e6cc11
Pratap Padhy <pratappadhy2005@gmail.com>
2026-03-25 20:30:24 +1100

    -Added Arrays example

---
+++ 02-basics/myarray.ts
@@ -0,0 +1,35 @@
+const superHeroes: string[] = ['spiderman', 'ironman', 'hulk'];
+superHeroes.push('thor');
+
+const heroPowers: number[] = [100, 200, 300];
+heroPowers.push(400);
+
+type User = {
+  name: string;
+  age: number;
+};
+
+const allUsers: User[] = [
+  {
+    name: 'prata',
+    age: 25,
+  },
+  {
+    name: 'sneha',
+    age: 24,
+  },
+];
+
+allUsers.push({
+  name: 'ananya',
+  age: 23,
+});
+
+const magicNumbers: Array<number> = [1, 2, 3, 4, 5];
+magicNumbers.push(6);
+
+// ML - Machine Learning
+const MLModels: number[][] = [
+    [1, 2, 3],
+    [4, 5, 6],
+]
